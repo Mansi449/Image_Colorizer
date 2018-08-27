a2d211e9eeec6cd66076a63427df5a3eafe44fe3
RITWIK JHA <ritwik0706@gmail.com>
2018-08-27 23:59:52 +0530

    Add Slider Module

--- app/src/main/java/mdg/com/imagecolorizer/MainActivity.java
+++ app/src/main/java/mdg/com/imagecolorizer/MainActivity.java
@@ -1,20 +1,14 @@
 package mdg.com.imagecolorizer;
 
 import android.Manifest;
-import android.annotation.SuppressLint;
 import android.app.Activity;
-import android.content.ContentUris;
-import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
-import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.drawable.Drawable;
 import android.net.Uri;
-import android.os.Build;
 import android.os.Bundle;
-import android.os.Environment;
-import android.provider.DocumentsContract;
 import android.provider.MediaStore;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
@@ -26,17 +20,15 @@ import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.Toast;
 
-import com.github.developer__.BeforeAfterSlider;
-
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.HttpURLConnection;
-import java.net.URISyntaxException;
 import java.util.Objects;
 
+import mdg.com.slidermodule.BeforeAfterSlider;
 import okhttp3.MediaType;
 import okhttp3.MultipartBody;
 import okhttp3.RequestBody;
