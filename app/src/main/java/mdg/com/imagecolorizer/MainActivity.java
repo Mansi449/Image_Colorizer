3e5bcb49aa9106dcc0aca18b344c3139f76e56aa
Mansi449 <mansi10022000@gmail.com>
2018-10-01 18:20:35 +0530

    Clean code

--- app/src/main/java/mdg/com/imagecolorizer/MainActivity.java
+++ app/src/main/java/mdg/com/imagecolorizer/MainActivity.java
@@ -1,7 +1,6 @@
 package mdg.com.imagecolorizer;
 
 import android.Manifest;
-import android.app.Activity;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
@@ -26,17 +25,7 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.HttpURLConnection;
-import java.util.Objects;
 
-import okhttp3.MediaType;
-import okhttp3.MultipartBody;
-import okhttp3.RequestBody;
-import okhttp3.ResponseBody;
-import retrofit2.Call;
-import retrofit2.Callback;
-import retrofit2.Response;
-import retrofit2.Retrofit;
-import retrofit2.converter.gson.GsonConverterFactory;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -44,63 +33,17 @@ public class MainActivity extends AppCompatActivity {
     private static final int CAMERA_REQUEST = 1888;
     Intent mintent=new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
     Uri uri;
-    private String filename;
     ImageView selected_image;
-    Bitmap bitmap;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        Button choose_img_from_gallery = findViewById(R.id.choose_img_from_gallery);
-        Button take_a_new_image = findViewById(R.id.take_a_new_image);
-        Button button_colorize = findViewById(R.id.buColorize);
-        Button  buSave = findViewById(R.id.buSave);
-        selected_image = findViewById(R.id.selected_image);
-
-        selected_image.setScaleType(ImageView.ScaleType.FIT_XY);
-
-        choose_img_from_gallery.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent();
-                // Show only images, no videos or anything else
-                intent.setType("image/*");
-                intent.setAction(Intent.ACTION_GET_CONTENT);
-                // Always show the chooser (if there are multiple options available)
-                startActivityForResult(Intent.createChooser(intent, "Select Picture"), PICK_IMAGE_REQUEST);
-
-            }
-        });
 
 
-        buSave.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                new Thread(new Runnable() {
-                    public void run() {
-
-                        Bitmap myBitmap = getBitmapfromURL("http://ec2-52-71-24-249.compute-1.amazonaws.com/colored/col_"+ filename + ".png");
-                        storeColoredImage(myBitmap);
-
-                    }
-                }).start();
-            }
-        });
         setContentView(R.layout.landing_page);
+        TextView choose_img_from_gallery = findViewById(R.id.choose_img_from_gallery);
 
-//        slider = findViewById(R.id.mySlider);
-//        progressBar = findViewById(R.id.progBar);
-        //  TextView choose_img_from_gallery = findViewById(R.id.choose_img_from_gallery);
-//        Button take_a_new_image = findViewById(R.id.take_a_new_image);
-//        Button button_colorize = findViewById(R.id.buColorize);
-//        Button  buSave = findViewById(R.id.buSave);
-//        selected_image = findViewById(R.id.selected_image);
-//
-//        selected_image.setScaleType(ImageView.ScaleType.FIT_XY);
-//
         choose_img_from_gallery.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -113,44 +56,6 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
-//        take_a_new_image.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                if (ContextCompat.checkSelfPermission(getApplicationContext(),
-//                        Manifest.permission.READ_CONTACTS)
-//                        != PackageManager.PERMISSION_GRANTED) {
-//
-//                    // Permission is not granted
-//                    requestThePermission();
-//                } else {
-//                    // Permission has already been granted
-//                    startActivity(mintent);
-//                    startActivityForResult(mintent, CAMERA_REQUEST);
-//                }
-//            }
-//        });
-//
-//        button_colorize.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                progressBar.setVisibility(View.VISIBLE);
-//                uploadImage();
-//            }
-//        });
-//
-//        buSave.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                new Thread(new Runnable() {
-//                    public void run() {
-//
-//                        Bitmap myBitmap = getBitmapfromURL("http://ec2-52-71-24-249.compute-1.amazonaws.com/colored/col_"+ filename + ".png");
-//                        storeColoredImage(myBitmap);
-//
-//                    }
-//                }).start();
-//            }
-//        });
 
     }
 
@@ -195,90 +100,5 @@ public class MainActivity extends AppCompatActivity {
                 MY_PERMISSIONS_REQUEST_CAMERA);
     }
 
-    private void storeUploadImage(Bitmap image) {
-        File pictureFile = getOutputMediaFile();
-        Bitmap newImage = Bitmap.createScaledBitmap(image, 512, 512, false);
-        if (pictureFile == null) {
-            Log.d("Error",
-                    "Error creating media file, check storage permissions: ");// e.getMessage());
-            return;
-        }
-        try {
-            FileOutputStream fos = new FileOutputStream(pictureFile);
-            newImage.compress(Bitmap.CompressFormat.JPEG, 90, fos);
-            fos.close();
-        } catch (FileNotFoundException e) {
-            Log.d("Error", "File not found: " + e.getMessage());
-        } catch (IOException e) {
-            Log.d("Error", "Error accessing file: " + e.getMessage());
-        }
-    }
-
-    private  File getOutputMediaFile(){
-
-        File mediaStorageDir = new File("/storage/emulated/0/Colorizer/");
-
-        if (! mediaStorageDir.exists()){
-            if (! mediaStorageDir.mkdirs()){
-                mediaStorageDir.mkdirs();
-            }
-        }
-
-        File mediaFile;
-        String mImageName="upload.jpg" ;
-        mediaFile = new File(mediaStorageDir.getPath() + File.separator + mImageName);
-        return mediaFile;
-    }
-
-    public Bitmap getBitmapfromURL(String src) {
-
-        Bitmap myBitmap;
-        try {
-            java.net.URL url = new java.net.URL(src);
-            HttpURLConnection connection = (HttpURLConnection) url
-                    .openConnection();
-            connection.setDoInput(true);
-            connection.connect();
-            InputStream input = connection.getInputStream();
-            myBitmap = BitmapFactory.decodeStream(input);
-            return myBitmap;
-        } catch (IOException e) {
-            e.printStackTrace();
-            return null;
-        }
-    }
-
-    private void storeColoredImage(Bitmap image) {
-        File pictureFile = getOutputDirectory();
-        if (pictureFile == null) {
-            Log.d("Error",
-                    "Error creating media file, check storage permissions: ");// e.getMessage());
-            return;
-        }
-        try {
-            FileOutputStream fos = new FileOutputStream(pictureFile);
-            image.compress(Bitmap.CompressFormat.JPEG, 100, fos);
-            fos.close();
-        } catch (FileNotFoundException e) {
-            Log.d("Error", "File not found: " + e.getMessage());
-        } catch (IOException e) {
-            Log.d("Error", "Error accessing file: " + e.getMessage());
-        }
-    }
-
-    private  File getOutputDirectory(){
-
-        File mediaStorageDir = new File("/storage/emulated/0/Colorizer/Coloured Images/");
-
-        if (! mediaStorageDir.exists()){
-            if (! mediaStorageDir.mkdirs()){
-                mediaStorageDir.mkdirs();
-            }
-        }
-
-        File mediaFile;
-        mediaFile = new File(mediaStorageDir.getPath() + File.separator + "col_" + filename + ".png");
-        return mediaFile;
-    }
 
 }
