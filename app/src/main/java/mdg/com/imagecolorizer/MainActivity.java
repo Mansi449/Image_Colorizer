19c6d4d880323fd896bf28a12685b8c03a0b9b7e
Mansi449 <mansi10022000@gmail.com>
2018-10-05 12:35:12 +0530

    add bottom sheet of sample images

--- app/src/main/java/mdg/com/imagecolorizer/MainActivity.java
+++ app/src/main/java/mdg/com/imagecolorizer/MainActivity.java
@@ -11,17 +11,22 @@ import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
+import android.support.design.widget.BottomSheetBehavior;
+import android.support.design.widget.BottomSheetDialog;
 
 public class MainActivity extends AppCompatActivity {
 
     private int PICK_IMAGE_REQUEST = 1;
     private static final int CAMERA_REQUEST = 1888;
-    Intent mintent=new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+    Intent mintent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
     Uri uri;
     ImageView selected_image;
-
+    LinearLayout layoutBottomSheet;
+    BottomSheetBehavior sheetBehavior;
+    View arrow;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,6 +34,9 @@ public class MainActivity extends AppCompatActivity {
 
         setContentView(R.layout.landing_page);
         TextView choose_img_from_gallery = findViewById(R.id.choose_img_from_gallery);
+        layoutBottomSheet = findViewById(R.id.bottom_sheet);
+        sheetBehavior = BottomSheetBehavior.from(layoutBottomSheet);
+        arrow = findViewById(R.id.vector);
 
         choose_img_from_gallery.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -43,10 +51,43 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        sheetBehavior.setBottomSheetCallback(new BottomSheetBehavior.BottomSheetCallback() {
+            @Override
+            public void onStateChanged(@NonNull View bottomSheet, int newState) {
+                switch (newState) {
+                    case BottomSheetBehavior.STATE_HIDDEN:
+                        break;
+                    case BottomSheetBehavior.STATE_EXPANDED: {
+                        // btnBottomSheet.setText("Close Sheet");
+                        arrow.setBackground(getDrawable(R.drawable.ic_expand_more_black_24dp));
+                        break;
+                    }
+
+                    case BottomSheetBehavior.STATE_COLLAPSED: {
+                        arrow.setBackground(getDrawable(R.drawable.ic_expand_less_black_24dp));
+                        break;
+                    }
+                    case BottomSheetBehavior.STATE_DRAGGING:
+                        arrow.setBackground(getDrawable(R.drawable.ic_expand_more_black_24dp));
+                        break;
+                    case BottomSheetBehavior.STATE_SETTLING:
+                        arrow.setBackground(getDrawable(R.drawable.ic_expand_less_black_24dp));
+                        break;
+                }
+            }
+
+
+            @Override
+            public void onSlide(@NonNull View bottomSheet, float slideOffset) {
+
+            }
+        });
+
+
     }
 
     @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+    protected void onActivityResult ( int requestCode, int resultCode, Intent data){
         super.onActivityResult(requestCode, resultCode, data);
 
         if (requestCode == PICK_IMAGE_REQUEST && resultCode == RESULT_OK && data != null && data.getData() != null) {
@@ -60,8 +101,8 @@ public class MainActivity extends AppCompatActivity {
     }
 
     @Override
-    public void onRequestPermissionsResult(int requestCode,
-                                           @NonNull String permissions[], @NonNull int[] grantResults) {
+    public void onRequestPermissionsResult (int requestCode,
+                                            @NonNull String permissions[], @NonNull int[] grantResults){
         switch (requestCode) {
             case 2: {
                 // If request is cancelled, the result arrays are empty.
@@ -79,12 +120,10 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    private void requestThePermission(){
+    private void requestThePermission () {
         int MY_PERMISSIONS_REQUEST_CAMERA = 2;
         ActivityCompat.requestPermissions(this,
                 new String[]{Manifest.permission.CAMERA},
                 MY_PERMISSIONS_REQUEST_CAMERA);
     }
-
-
 }
